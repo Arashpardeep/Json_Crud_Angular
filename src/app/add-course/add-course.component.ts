bf1a491a28066012366a04a4568a719edb205229
Arashpardeep Singh <singharash571@gmail.com>
2023-12-12 15:15:04 +0530

    newCommit

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { CrudService } from '../crud.service';
 
 @Component({
   selector: 'app-add-course',
   templateUrl: './add-course.component.html',
-  styleUrls: ['./add-course.component.css']
+  styleUrls: ['./add-course.component.css'],
 })
 export class AddCourseComponent {
-  userform = new FormGroup({ 
-    courseid: new FormControl(),
-    coursename: new FormControl(),
+  userform = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
   });
 
+  constructor(private service: CrudService) {}
+
   submitform() {
-    console.log(this.userform.value);
+    console.log('Form submitted:', this.userform.value);
+    this.service.AddCourse(this.userform.value).subscribe();
   }
 }
--- src/app/crud.service.ts
+++ src/app/crud.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,13 +8,28 @@ export class CrudService {
 
   url = "https://jsonplaceholder.typicode.com/users";
   url1 = "http://localhost:3000/courses";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   constructor(private client: HttpClient) { }
 
   GetUsers(): any{
     return this.client.get(this.url); //always return 'Observable' datatype
   }
 
-  GetCourses():any{
+  GetCourses() {
     return this.client.get(this.url1);
+  }
+  
+  AddCourse(course: any) {
+    return this.client.post(
+      this.url1,
+      JSON.stringify(course),
+      this.httpOptions
+    );
   }
 }
